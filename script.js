9f31d3cf62d61b1c7b2f14dec94ae0b637bb76b7
mohammad Alhasan <Alhasan.lap@outlook.com>
2022-09-25 13:09:36 +0200

    Be a lutos member

---
+++ script.js
@@ -0,0 +1,8 @@
+function goFurther(){
+
+   if (document.getElementById("flexCheckDefault").checked == true)
+   document.getElementById("submit").disabled = false;
+   else
+   document.getElementById("submit").disabled = true;
+   }
+   
